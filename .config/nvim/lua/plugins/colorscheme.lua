29b66de5f9b6072c74c0718e830261bece498bfd
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-06-02 18:53:04 +0900

    feat:選択業のハイライトを透明に

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,5 @@
 return {
-  -- tokyonightを無効化
-  -- { "folke/tokyonight.nvim", enabled = false },
-
+  { "folke/tokyonight.nvim", enabled = false },
   -- onedarkを追加
   {
     "navarasu/onedark.nvim",
@@ -14,6 +12,7 @@ return {
         toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
       })
       require("onedark").load()
+      vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
     end,
   },
 }
