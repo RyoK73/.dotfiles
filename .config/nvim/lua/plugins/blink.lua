ca4f6ce3f9b863eb2217baf59df18b02679c720a
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-06-05 02:36:57 +0900

    feat:bufferからの補完を追加修正
    - providersからbufferを無効化
    - コメントアウト部を削除

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -5,38 +5,11 @@ return {
       ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
       ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
     },
+    sources = {
+      default = { "lsp", "path", "snippets" },
+      providers = {
+        buffer = { enabled = false },
+      },
+    },
   },
-  -- opts = function(_, opts)
-  --   opts.keymap = opts.keymap or {}
-  --   opts.keymap["<Tab>"] = {
-  --     function(cmp)
-  --       if cmp.is_visible() then
-  --         return cmp.select_next()
-  --       end
-  --     end,
-  --     function()
-  --       vim.api.nvim_feedkeys(
-  --         vim.api.nvim_replace_termcodes("<C-t>", true, true, true),
-  --         "n",
-  --         false
-  --       )
-  --       return true
-  --     end,
-  --   }
-  --   opts.keymap["<S-Tab>"] = {
-  --     function(cmp)
-  --       if cmp.is_visible() then
-  --         return cmp.select_prev()
-  --       end
-  --     end,
-  --     function()
-  --       vim.api.nvim_feedkeys(
-  --         vim.api.nvim_replace_termcodes("<C-d>", true, true, true),
-  --         "n",
-  --         false
-  --       )
-  --       return true
-  --     end,
-  --   }
-  -- end,
 }
