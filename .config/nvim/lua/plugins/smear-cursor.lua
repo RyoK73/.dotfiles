0de954a9c2c662e3713accf1e3b7d3dcca83f845
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-06-07 15:17:16 +0900

    fix:particlesが文字に重ならないよう修正

--- .config/nvim/lua/plugins/smear-cursor.lua
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -11,14 +11,15 @@ return {
     gamma = 1,
     never_draw_over_target = true, -- if you want to actually see under the cursor
     hide_target_hack = true, -- same
-    particle_spread = 1,
+    particle_spread = 0.4,
     particles_per_second = 500,
     particles_per_length = 50,
     particle_max_lifetime = 1000,
     particle_max_initial_velocity = 20,
     particle_velocity_from_cursor = 0.5,
     particle_damping = 0.15,
-    particle_gravity = -50,
+    particle_gravity = -20,
+    particles_over_text = false,
     min_distance_emit_particles = 0,
     smear_insert_mode = false,
   },
