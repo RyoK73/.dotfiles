b54c5f4e8bb53bcb4d9a1a1b59dee33c5c8c1b73
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-06-09 11:18:08 +0900

    feat:smear-cursorのparticlesがinsertモードで無効になる機能を追加

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -3,3 +3,14 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
   command = "setlocal nospell",
 })
+-- smear-cursor insertモードではparticlesをオフに、ノーマルモードではオンに
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = function()
+    require("smear_cursor.config").particles_enabled = false
+  end,
+})
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    require("smear_cursor.config").particles_enabled = true
+  end,
+})
