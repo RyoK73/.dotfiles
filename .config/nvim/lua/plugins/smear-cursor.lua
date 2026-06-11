f84b53022746be7587098eaa4281b2981b68cebe
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-06-11 10:23:03 +0900

    fix:particlesが無効化できていなかったため追加修正

--- .config/nvim/lua/plugins/smear-cursor.lua
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -1,7 +1,7 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {
-    cursor_color = "#01d6e4",
+    -- cursor_color = "#01d6e4",
     particles_enabled = false, -- 描画が重いので一旦false
     stiffness = 0.5,
     trailing_stiffness = 0.2,
@@ -11,16 +11,16 @@ return {
     gamma = 1,
     never_draw_over_target = true, -- if you want to actually see under the cursor
     hide_target_hack = true, -- same
-    particle_spread = 0.4,
-    particles_per_second = 500,
-    particles_per_length = 50,
-    particle_max_lifetime = 1000,
-    particle_max_initial_velocity = 20,
-    particle_velocity_from_cursor = 0.5,
-    particle_damping = 0.15,
-    particle_gravity = -20,
-    particles_over_text = false,
-    min_distance_emit_particles = 0,
+    -- particle_spread = 0.4,
+    -- particles_per_second = 500,
+    -- particles_per_length = 50,
+    -- particle_max_lifetime = 1000,
+    -- particle_max_initial_velocity = 20,
+    -- particle_velocity_from_cursor = 0.5,
+    -- particle_damping = 0.15,
+    -- particle_gravity = -20,
+    -- particles_over_text = false,
+    -- min_distance_emit_particles = 0,
     smear_insert_mode = false,
   },
 }
