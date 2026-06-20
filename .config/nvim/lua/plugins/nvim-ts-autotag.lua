08cec0bf774446122aa7551228a2099032213485
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-06-20 13:30:17 +0900

    fix:mdファイルでタグが自動で閉じる設定を無効化

--- .config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ .config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,12 +1,11 @@
 return {
   "windwp/nvim-ts-autotag",
   opts = {
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = false, -- Auto close on trailing </
     per_filetype = {
       ["markdown"] = {
         enable_close = false,
+        enable_rename = false,
+        enable_close_on_slash = false,
       },
     },
   },
