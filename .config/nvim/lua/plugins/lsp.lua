2061bd2d3f4c0e255016cf4e35eeefff2d54f4bf
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-07-17 11:45:51 +0900

    fix: TypeScriptのimportがblink.cmpで解決されない問題を修正開始
    - 未修正

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,18 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    servers = {
+      vtsls = {
+        settings = {
+          typescript = {
+            preferences = {
+              includeCompletionsForModuleExports = true,
+              includeCompletionsForImportStatements = true,
+            },
+          },
+        },
+      },
+    },
     vim.lsp.enable("postgres_lsp"),
   },
 }
--- .config/nvim/lua/plugins/suggestion.lua
+++ .config/nvim/lua/plugins/suggestion.lua
@@ -1,10 +1,12 @@
 return {
   {
     "saghen/blink.cmp",
+    lazy = false,
     opts = {
       keymap = {
         ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+        ["<C-Space>"] = { "show" },
       },
       sources = {
         default = { "lsp", "path" },
