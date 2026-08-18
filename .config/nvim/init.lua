f634d13121031f37fd91995b99a7d91400527c42
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-08-18 10:16:26 +0900

    feat: 背景の透過設定を追加

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,10 +6,25 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 
--- シェル系だけタブに
+-- シェル系だけTabでインデントに
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "sh", "zsh" },
   callback = function()
     vim.opt_local.expandtab = false
   end,
 })
+
+-- 背景を透過
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+-- vim.cmd([[ highlight Normal guibg=none
+--   highlight NonText guibg=none
+--   highlight Normal ctermbg=none
+--   highlight NonText ctermbg=none
+--   highlight NormalNC guibg=none
+--   highlight NormalSB guibg=none
+-- ]])
